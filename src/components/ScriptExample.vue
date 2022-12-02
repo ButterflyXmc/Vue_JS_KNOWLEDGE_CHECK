c59375f318e25fb1b5d9070929f912a9823081ca
mahic <mahic@domain.com>
2022-12-01 20:36:33 -0700

    KC done

--- src/components/ScriptExample.vue
+++ src/components/ScriptExample.vue
@@ -1,14 +1,50 @@
 <template>
   <div>
-    <h1>Hello</h1>
+    <h1>W13E Vue JS KNOWLEDGE CHECKS</h1>
   </div>
 </template>
 
 <script>
+// Add the following variables to your component:
+// 1. tweets: Array of strings
+// 2. num_friends: Number
+// 3. is_logged_in: Boolean
 export default {
-  tweets: ["Tweet1", "Tweet2", "Tweet3"],
-  numFriends: 3,
-  loggedIn: true,
+  name: "ScriptExample",
+  data() {
+    return {
+      tweets: ["Tweet1", "Tweet2", "Tweet3", "Tweet4"],
+      numFriends: 10,
+      isLoggedIn: true,
+    };
+  },
+
+  // pt2 Create a function called showInfo and achieve the following :
+  // 1. Inject the num_friends into a tag
+  // 2. Inject a tag that reflects the user being logged in or not
+  // 3. Inject each tweet into a tag.
+  // mounting needs method, so we have to make a method first
+  methods: {
+    showInfo() {
+      console.log("Showing info");
+      // making a const so we don't repeat
+      const myDiv = document.querySelector(`div`);
+      // ------
+      myDiv.insertAdjacentHTML(`beforeend`, `<h2>${this.numFriends}</h2>`);
+      if (this.isLoggedIn) {
+        myDiv.insertAdjacentHTML(`beforeend`, `<h3>Welcome to the page</h3>`);
+      } else {
+        myDiv.insertAdjacentHTML(`beforeend`, `<h3>User is not logged in</h3>`);
+      }
+      for (let tweet of this.tweets) {
+        myDiv.insertAdjacentHTML(`beforeend`, `<p>${tweet}</p>`);
+      }
+    },
+  },
+  // we need to call it in the function for the method to work
+  mounted() {
+    this.showInfo();
+  },
 };
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // TO ADD A BREAKPOINT on chrome to vue components! 
+  // ONLY NEEDS TO BE DONE ONCE PER PROJECT!!
+  // add another object 
+  // on chrome debugger.. go to sources
+  configureWebpack: {
+    devtool: `source-map`
+  }
 })
